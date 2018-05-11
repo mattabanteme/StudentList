89cf87825fb6b7ec7c8ecb4a67ee449a26a066c2
kpfister <kpfister@me.com>
2018-05-11 14:27:27 -0600

    Added new-new print statment

--- Sources/AppDelegate.swift
+++ Sources/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	var window: UIWindow?
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-		
+		print("Nick is awesome")
         print("Application started successfully")
 		return true
 	}
