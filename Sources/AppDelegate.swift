bcf68a6ab79aef32ec9f753e6cc87b7132e31d92
kpfister <kpfister@me.com>
2018-05-11 14:21:14 -0600

    Remove print statement

--- Sources/AppDelegate.swift
+++ Sources/AppDelegate.swift
@@ -16,7 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 		
         print("Application started successfully")
-        print("Karl is the greatest")
 		return true
 	}
 }
